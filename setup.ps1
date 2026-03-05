77f4a2cd2e2a17d1edeffee0bfe6fcf0d402baeb
Rotem Ravid <rotem@COMP34.TIKTIN2017.local>
2026-03-06 01:50:04 +0200

    added verifications and fixed bugs

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,12 @@
 $zip = "$env:TEMP\BakkesModSetup.zip"
 $out = "$env:TEMP\BakkesModSetup"
 
+Write-Host ""
+Write-Host "  >> BakkesMod Setup and Configuration - rotmvrr "
+Write-Host "========================================"
+Write-Host ""
+
+
 if( Test-Path "$env:APPDATA\bakkesmod\bakkesmod\version.txt") {
     Write-Host "BM already exists" -ForegroundColor Yellow
 } else {
@@ -10,10 +16,20 @@ Expand-Archive $zip -DestinationPath $out -Force
 Start-Process "$out\BakkesModSetup.exe" -ArgumentList "/silent" -Wait
 }
 
+Write-Host ""
+Write-Host ""
+Write-Host ""
 Write-Host "installing AlphaConsole..." -ForegroundColor Cyan
 Start-Process "bakkesmod://install/108"
 
+Write-Host ""
+Write-Host ""
 Write-Host "setting default configs (rotmvrr's cfg)" -ForegroundColor Cyan
-Copy-Item -Path "$PSScriptRoot\configs\bakkesmod\*" -Destination "$env:APPDATA\bakkesmod\bakkesmod\" -Recurse -Force
+Copy-Item -Path "$PSScriptRoot\configs\bakkesmod\cfg\_Presets.json" -Destination "$env:APPDATA\bakkesmod\bakkesmod\data\acplugin\Presets" -Recurse -Force
 
+Write-Host ""
+Write-Host ""
+Write-Host ""
+Write-Host ""
 Write-Host "* READY TO LAUNCH *" -ForegroundColor Green
+Read-Host "Press enter to exit the installation"
